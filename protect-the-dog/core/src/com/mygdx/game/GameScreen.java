4715ddb0cc02ce2e5678328691d39d70ade17a2e
ArditGashi <sejeardit@hotmail.com>
2022-05-11 17:24:14 +0200

    load tmx tile map

--- protect-the-dog/core/src/com/mygdx/game/GameScreen.java
+++ protect-the-dog/core/src/com/mygdx/game/GameScreen.java
@@ -6,10 +6,12 @@ import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import helper.TileMapHelper;
 
 import static helper.Constants.PPM;
 
@@ -19,11 +21,17 @@ public class GameScreen extends ScreenAdapter {
     private World world;
     private Box2DDebugRenderer box2DDebugRenderer;
 
+    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+    private TileMapHelper tileMapHelper;
+
     public GameScreen(OrthographicCamera camera) {
         this.camera = camera;
         this.batch = new SpriteBatch();
         this.world = new World(new Vector2(0, 0), false);
         this.box2DDebugRenderer = new Box2DDebugRenderer();
+
+        this.tileMapHelper = new TileMapHelper();
+        this.orthogonalTiledMapRenderer = tileMapHelper.setupMap();
 
 
     }
@@ -31,10 +39,11 @@ public class GameScreen extends ScreenAdapter {
     private void update() {
         world.step(1 / 60f, 6, 2);
 
-        cameraUpdate();
+        cameraUpdate(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
 
 
         batch.setProjectionMatrix(camera.combined);
+        orthogonalTiledMapRenderer.setView(camera);
 
         if(Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
             Gdx.app.exit();
@@ -42,8 +51,11 @@ public class GameScreen extends ScreenAdapter {
 
     }
 
-    private void cameraUpdate() {
-        camera.position.set(new Vector3(0, 0, 0));
+    private void cameraUpdate(int width, int height) {
+        camera.viewportHeight = height;
+        camera.viewportWidth = width;
+        camera.position.set(width/2f, height/2f, 0); //by default camera position on (0,0,0)
+//        camera.position.set(new Vector3(0, 0, 0));
         camera.update();
 
     }
@@ -54,6 +66,8 @@ public class GameScreen extends ScreenAdapter {
 
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        orthogonalTiledMapRenderer.render();
 
         batch.begin();
         // render objects
