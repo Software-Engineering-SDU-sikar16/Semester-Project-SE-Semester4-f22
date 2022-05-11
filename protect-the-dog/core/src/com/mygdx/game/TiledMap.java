f96d4e7fae40f683911ad320a9c0302bbd931917
Simon <simonkaring@gmail.com>
2022-05-11 16:27:54 +0200

    doing some map stuff

---
+++ protect-the-dog/core/src/com/mygdx/game/TiledMap.java
@@ -0,0 +1,36 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import screens.GameScreen;
+
+public class TiledMap extends Game {
+    @Override
+    public void create() {
+        setScreen(new GameScreen());
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+
+    @Override
+    public void render() {
+        super.render();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        super.resize(width, height);
+    }
+
+    @Override
+    public void pause() {
+        super.pause();
+    }
+
+    @Override
+    public void resume() {
+        super.resume();
+    }
+}
--- protect-the-dog/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ protect-the-dog/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -2,11 +2,16 @@ package com.mygdx.game.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.mygdx.game.MyGdxGame;
+//import com.mygdx.game.MyGdxGame;
+import com.mygdx.game.TiledMap;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxGame(), config);
+		config.title = "Gaem";
+		config.width = 1280;
+		config.height = 720;
+//		new LwjglApplication(new MyGdxGame(), config);
+		new LwjglApplication(new TiledMap(), config);
 	}
 }
