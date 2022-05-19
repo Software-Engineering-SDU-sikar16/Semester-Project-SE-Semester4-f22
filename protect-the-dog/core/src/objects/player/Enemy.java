f7a9c2bee861c4f131336c703a45956005d7f274
ArditGashi <sejeardit@hotmail.com>
2022-05-19 19:25:35 +0200

    protect the dog enemy movement

--- protect-the-dog/core/src/objects/player/Enemy.java
+++ protect-the-dog/core/src/objects/player/Enemy.java
@@ -23,7 +23,7 @@ public class Enemy extends GameEntity{
         y = body.getPosition().y * PPM;
         this.getSprite().setPosition(x, y);
 
-        checkUserInput();
+//        checkUserInput();
         moveEnemy();
 
     }
@@ -33,41 +33,85 @@ public class Enemy extends GameEntity{
 
     }
 
-    private void checkUserInput() {
-        velX = 0;
-        velY = 0;
-        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
-            velX = 1;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
-            velX = -1;
-        }
-
-        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
-            velY = -1;
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
-            velY = 1;
-        }
-
-
-
-
-//        if (Gdx.input.isKeyPressed(Input.Keys.SPACE) && jumpCounter < 2) {
-//            float force = body.getMass() * 18;
-//            body.applyLinearImpulse(new Vector2(0, force), body.getPosition(), true);
+//    private void checkUserInput() {
+//        velX = 0;
+//        velY = 0;
+//        if (Gdx.input.isKeyPressed(Input.Keys.D)) {
+//            velX = 1;
 //        }
-        body.setLinearVelocity(velX * speed, velY* speed);
-
-    }
+//        if (Gdx.input.isKeyPressed(Input.Keys.A)) {
+//            velX = -1;
+//        }
+//
+//        if (Gdx.input.isKeyPressed(Input.Keys.S)) {
+//            velY = -1;
+//        }
+//        if (Gdx.input.isKeyPressed(Input.Keys.W)) {
+//            velY = 1;
+//        }
+//
+//        body.setLinearVelocity(velX * speed, velY* speed);
+//
+//    }
 
     private void moveEnemy(){
-        if(body.getPosition().x < 0){
-            body.setLinearVelocity(-speed, 0);
+        System.out.println("move enemy");
+        //System.out.println(body.getPosition());
+        System.out.println(body.getWorldCenter());
+
+        //check if enemy is not moving
+        if(body.getLinearVelocity().x == 0 && body.getLinearVelocity().y == 0){
+            System.out.println("enemy is not moving");
         }
-        if(body.getPosition().x > 0){
-            body.setLinearVelocity(speed, 0);
+
+        // check if enemy is not moving position
+        if(body.getPosition().x == body.getWorldCenter().x && body.getPosition().y == body.getWorldCenter().y){
+            System.out.println("enemy is not moving position");
+            body.setLinearVelocity(1, 0);
+        } else {
+            body.setLinearVelocity(0, 1); // move enemy down
         }
+
+        // if enemy is not moving position move the enemy down or right
+        if(body.getPosition().x == body.getWorldCenter().x && body.getPosition().y == body.getWorldCenter().y){
+            body.applyLinearImpulse(0, -0.1f, body.getWorldCenter().x, body.getWorldCenter().y, true);
+        }
+
+
+        //create a checker which checks if body.getPosition is the same for more than 2 seconds
+
+
+
+       // move body 2 pixels to the right
+        //body.setTransform(20, body.getPosition().y, 0);
+       // body.setLinearVelocity(2, -1);
+        // body.setAngularVelocity(-8);
+        // body.setTransform(body.getWorldCenter().x + 0.1f, body.getWorldCenter().y, 0);
+
+       // body.setLinearVelocity(0, -1); move down
+
+//        if (body.getPosition().y < 0.5f){
+//            body.setLinearVelocity(0, 1);   //move up
+//        }
+
+
+
+
+
+//        if(body.getPosition().x < 0){
+//            body.setLinearVelocity(-speed, 0);
+//        } else if(body.getPosition().x > 0){
+//            body.setLinearVelocity(speed, 0);
+//        } else if(body.getPosition().y < 0){
+//            body.setLinearVelocity(0, -speed);
+//        } else if(body.getPosition().y > 0){
+//            body.setLinearVelocity(0, speed);
+//        }
+
+        // check if enemy is not moving
+
+
+        // if enemy can not go right go dow
     }
 
 
