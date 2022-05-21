b6927f5703e47fcdd7855b4ace3ae4e1f5a6a589
Simon <simonkaring@gmail.com>
2022-05-21 17:39:42 +0200

    Merge branch 'osgi-moduling' into main

---
+++ TowerDefenseOSGi/OSGiMap/src/main/java/dk/sdu/mmmi/cbse/osgimap/MapService.java
@@ -0,0 +1,35 @@
+package dk.sdu.mmmi.cbse.osgimap;
+
+
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import dk.sdu.mmmi.cbse.common.services.IMapService;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class MapService implements IMapService {
+    private TiledMap tiledMap;
+    private OrthogonalTiledMapRenderer orthogonalTiledMapRenderer;
+    @Override
+    public void createMap() {
+//        Path currentRelativePath = Paths.get("..");
+//        String s = currentRelativePath.toAbsolutePath().toString();
+//        System.out.println("Current absolute path is: " + s);
+        tiledMap = new TmxMapLoader().load("../assets/maps/map1.tmx");
+        orthogonalTiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+
+    }
+
+    @Override
+    public void updateMap(OrthographicCamera camera) {
+        if (orthogonalTiledMapRenderer == null) {
+            createMap();
+        }
+        orthogonalTiledMapRenderer.setView(camera);
+        orthogonalTiledMapRenderer.render();
+    }
+
+}
