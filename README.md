5757d8cd80949512683fff02ad3a2a60a193b623
Simon <simonkaring@gmail.com>
2022-05-21 17:07:58 +0200

    Edited mygdxgame

--- README.md
+++ README.md
@@ -5,12 +5,15 @@ https://www.mapeditor.org/
 
 
 ## How to run protect-the-dog
+Gradle:
+- build `build.gradle.` 
 Just run DesktopLauncher
 
 ## How to run with OSGi in IntelliJ
+- 
 - In file -> project structure, set SDK to 1.8
 - Add new run configuration from top right.
 - Choose maven and give it a name
 - Choose the project directory 
-- Write `install pas:provision` in the command line parameter
+- Write `install pax:provision` in the command line parameter
 - Now ya gucci to run
--- protect-the-dog/core/src/com/mygdx/game/MyGdxGame.java
+++ protect-the-dog/core/src/com/mygdx/game/MyGdxGame.java
@@ -22,14 +22,54 @@ public class MyGdxGame extends Game {
 	}
 
 	@Override
-	public void create () {
-		this.widthScreen = Gdx.graphics.getWidth();
-		this.heightScreen = Gdx.graphics.getHeight();
-		this.orthographicCamera = new OrthographicCamera();
-		this.orthographicCamera.setToOrtho(false, widthScreen, heightScreen);
-		setScreen(new GameScreen(orthographicCamera));
-
-
+	public void create()
+	{
+		Constants.GlobalWidth = Gdx.graphics.getWidth();
+		Constants.GlobalHeight = Gdx.graphics.getHeight();
+		Resources.LoadFonts();
+		Resources.LoadTextures();
+		Resources.LoadSounds();
+	
+		
+		Constants.Viewport = new FitViewport(Constants.GlobalWidth, Constants.GlobalHeight);
+		Constants.Stage = new Stage(Constants.Viewport);
+		Gdx.input.setInputProcessor(Constants.Stage);
+		
+		new CustomCursorDrawer();
+		
+		
+		Constants.Camera = new OrthographicCamera();
+		Constants.Camera.setToOrtho(false, Constants.GlobalWidth, Constants.GlobalHeight);
+		setScreen(new GameScreen());
+		
+		
+		// Create Overlays
+		Constants.PauseScreen = new PauseScreen();
+		Constants.GameUIOverlay = new GameUIOverlay();
+		
+		
+		for (int i = 0; i < 23; i++)
+		{
+			AnimatedSprite sprite2 = new AnimatedSprite(50 + (i * 50), 158 + (i), 24, 24);
+//			sprite2.AddAnimation("default", Resources.LoadTexture("entities/enemies/enemies/sheet_"+ i + ".png "), 1, 4, 12, Animation.PlayMode.NORMAL);
+		}
+		
+		
+		sprite = new AnimatedSprite("spine/FogExplosion/fog explosion.atlas", "spine/FogExplosion/skeleton.json", 128, 128);
+		sprite.SetZIndex(2);
+		
+		
+		
+		// these must be the last things that happen when the application is created, after everything else as possible.
+		Entity.CreateAllEntities();
+		
+		Overlay.CreateAllOverlays();
+		
+		Constants.shapeRenderer = new ShapeRenderer();
+		Constants.batch = new SpriteBatch();
+		Constants.font = new BitmapFont();
+		
+		
 	}
 
 
