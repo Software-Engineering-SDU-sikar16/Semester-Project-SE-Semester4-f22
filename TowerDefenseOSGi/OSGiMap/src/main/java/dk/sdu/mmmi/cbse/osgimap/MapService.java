1751fb3ff465340b00618fa528129a8b475e2625
Simon <simonkaring@gmail.com>
2022-05-23 12:57:04 +0200

    Fix libgdx versions and compilation fix

--- TowerDefenseOSGi/OSGiMap/src/main/java/dk/sdu/mmmi/cbse/osgimap/MapService.java
+++ TowerDefenseOSGi/OSGiMap/src/main/java/dk/sdu/mmmi/cbse/osgimap/MapService.java
@@ -41,50 +41,50 @@ public class MapService implements IMapService {
         orthogonalTiledMapRenderer.render();
     }
 
-    public void CalculateGraph()
-    {
-        Constants.GameMapGraph = new GameMapGraph();
-
-        Tile previousTile = null;
-
-        MapLayer layer = tiledMap.getLayers().get("PathLayer");
-
-        MapObjects mapObjects = layer.getObjects();
-
-        for (MapObject mapObject : mapObjects)
-        {
-            if (mapObject instanceof RectangleMapObject)
-            {
-                RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject;
-                // now you can get the position of the rectangle like this:
-                Rectangle rectangle = rectangleMapObject.getRectangle();
-                int x = (int) rectangle.getX();
-                int y = (int) rectangle.getY();
-
-                // create a graph tile for each object.
-                Tile graphTile = new Tile(x, y, rectangleMapObject.getName());
-                Constants.GameMapGraph.AddTile(graphTile);
-
-                EnemyPath.put(new Vector2(x, y), graphTile);
-
-                if (Start == null)
-                {
-                    Start = graphTile;
-                }
-
-                if (previousTile != null)
-                {
-                    Constants.GameMapGraph.ConnectTiles(previousTile, graphTile);
-                }
-                previousTile = graphTile;
-
-            }
-        }
-
-        End = previousTile;
-        EnemyPath.put(new Vector2(End.x, End.y), End);
-        Constants.GameMapPath = Constants.GameMapGraph.FindPath(Start, End);
-
-    }
+//    public void CalculateGraph()
+//    {
+//        Constants.GameMapGraph = new GameMapGraph();
+//
+//        Tile previousTile = null;
+//
+//        MapLayer layer = tiledMap.getLayers().get("PathLayer");
+//
+//        MapObjects mapObjects = layer.getObjects();
+//
+//        for (MapObject mapObject : mapObjects)
+//        {
+//            if (mapObject instanceof RectangleMapObject)
+//            {
+//                RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject;
+//                // now you can get the position of the rectangle like this:
+//                Rectangle rectangle = rectangleMapObject.getRectangle();
+//                int x = (int) rectangle.getX();
+//                int y = (int) rectangle.getY();
+//
+//                // create a graph tile for each object.
+//                Tile graphTile = new Tile(x, y, rectangleMapObject.getName());
+//                Constants.GameMapGraph.AddTile(graphTile);
+//
+//                EnemyPath.put(new Vector2(x, y), graphTile);
+//
+//                if (Start == null)
+//                {
+//                    Start = graphTile;
+//                }
+//
+//                if (previousTile != null)
+//                {
+//                    Constants.GameMapGraph.ConnectTiles(previousTile, graphTile);
+//                }
+//                previousTile = graphTile;
+//
+//            }
+//        }
+//
+//        End = previousTile;
+//        EnemyPath.put(new Vector2(End.x, End.y), End);
+//        Constants.GameMapPath = Constants.GameMapGraph.FindPath(Start, End);
+//
+//    }
 
 }
