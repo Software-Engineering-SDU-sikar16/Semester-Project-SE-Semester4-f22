c1eeaa3c4a4024b28a26d8bb22ad714569ca1af6
Simon <simonkaring@gmail.com>
2022-05-27 11:17:25 +0200

    Started with AnimatedSpritePart (not working)

--- TowerDefenseOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/TimerPart.java
+++ TowerDefenseOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/TimerPart.java
@@ -8,8 +8,7 @@ package dk.sdu.mmmi.cbse.common.data.entityparts;
 import dk.sdu.mmmi.cbse.common.data.Entity;
 import dk.sdu.mmmi.cbse.common.data.GameData;
 
-public class TimerPart
-        implements EntityPart {
+public class TimerPart implements EntityPart {
 
     private float expiration;
 
