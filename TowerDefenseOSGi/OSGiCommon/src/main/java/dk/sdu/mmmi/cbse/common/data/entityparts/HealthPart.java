f6b1844e0b56cb2f57776e8dc904a22e8aee9606
Simon Karing <simonkaring@gmail.com>
2022-05-29 00:51:06 +0200

    health part

---
+++ TowerDefenseOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/HealthPart.java
@@ -0,0 +1,12 @@
+/*package dk.sdu.mmmi.cbse.common.data.entityparts;
+
+import com.badlogic.gdx.graphics.Texture;
+
+public class HealthPart implements EntityPart {
+    public static Texture HeartTexture = Resources.LoadTexture("ui/hp.png");
+    public static Texture HeartTextureNull = Resources.LoadTexture("ui/hp_null.png");
+    int healthCounter = 0;
+
+
+}
+*/
