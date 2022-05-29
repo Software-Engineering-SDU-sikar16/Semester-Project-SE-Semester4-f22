b84ed24ce36f4e8d515ec7b6486176db60408de2
Simon Karing <simonkaring@gmail.com>
2022-05-29 03:49:11 +0200

    healthpart

---
+++ TowerDefenseOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/Util.java
@@ -0,0 +1,22 @@
+package dk.sdu.mmmi.cbse.common.data;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Vector2;
+
+public class Util
+{
+
+    public static float Clamp(float value, float min, float max)
+    {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static int Clamp(int value, int min, int max)
+    {
+        return Math.max(min, Math.min(max, value));
+    }
+
+    public static double distance(Vector2 object1, Vector2 object2)
+    {
+        return Math.sqrt(Math.pow((object2.x - object1.x), 2) + Math.pow((object2.y - object1.y), 2));
+    }
+}
--- TowerDefenseOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/HealthPart.java
+++ TowerDefenseOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/entityparts/HealthPart.java
@@ -1,12 +1,86 @@
-/*package dk.sdu.mmmi.cbse.common.data.entityparts;
+package dk.sdu.mmmi.cbse.common.data.entityparts;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import dk.sdu.mmmi.cbse.common.data.Entity;
+import dk.sdu.mmmi.cbse.common.data.GameData;
+import dk.sdu.mmmi.cbse.common.data.Resources;
 
 public class HealthPart implements EntityPart {
     public static Texture HeartTexture = Resources.LoadTexture("ui/hp.png");
     public static Texture HeartTextureNull = Resources.LoadTexture("ui/hp_null.png");
-    int healthCounter = 0;
+    int currentHealth;
+    int startHealth;
+    Sprite[] heartTextures;
 
+    public HealthPart(int x, int y, int startHealth){
+        this.startHealth = startHealth;
+        currentHealth = startHealth;
+        UpdateHeartTextureList();
+    }
+    public int GetHealth()
+    {
+        return currentHealth;
+    }
+
+    public void AddHealth(int amount){
+        if (IsDead()){
+            return;
+        }
+        if (currentHealth + amount > startHealth){
+            return;
+        }
+        currentHealth += amount;
+        UpdateHeartTextureList();
+    }
+    public void SubtractHealth(int amount){
+        currentHealth -= amount;
+        if (!IsDead()){
+            UpdateHeartTextureList();
+        }
+    }
+
+    public void UpdateHeartTextureList(){
+        heartTextures = new Sprite[startHealth];
+        for (int i = 0; i < startHealth; i++){
+            if (i <= currentHealth) {
+                heartTextures[i] = new Sprite(HeartTexture);
+
+            } else {
+                heartTextures[i] = new Sprite(HeartTextureNull);
+            }
+        }
+    }
+
+    public boolean IsDead()
+    {
+        return currentHealth <= 0;
+    }
+
+    public void SetSize(int width, int height){
+        for (Sprite heart : heartTextures){
+            heart.setSize(width, height);
+        }
+    }
+
+    public void SetPosition(float width, float height){
+        for (Sprite heart : heartTextures){
+            heart.setPosition(width, height);
+        }
+    }
+    @Override
+    public void process(GameData gameData, Entity entity) {
+        gameData.GlobalSpriteBatch.begin();
+        if (entity.getTexture() != null){
+            for (int i = 0; i < heartTextures.length; i++) {
+                float heartX = entity.getX() + i*30;
+                float heartY = entity.getY() - 15;
+                heartTextures[i].setPosition(heartX, heartY);
+                gameData.GlobalSpriteBatch.draw(heartTextures[i].getTexture(), heartTextures[i].getX(), heartTextures[i].getY(), heartTextures[i].getWidth(), heartTextures[i].getHeight());
+            }
+
+        }
+        gameData.GlobalSpriteBatch.end();
+    }
 
 }
-*/
