cd5d691f7bf9fdab5bd62ece9f413f1c8da6b520
ArditGashi <sejeardit@hotmail.com>
2022-06-24 10:18:20 +0200

    clean up unused code

--- TowerDefenseOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/Map/TileMapHelper.java
+++ TowerDefenseOSGi/OSGiCommon/src/main/java/dk/sdu/mmmi/cbse/common/data/Map/TileMapHelper.java
@@ -45,62 +45,62 @@ public class TileMapHelper {
         TilePixelHeight = prop.get("tileheight", Integer.class);
         System.out.println("Tile Width: " + TilePixelWidth);
 
-        MapPixelWidth = MapWidth * TilePixelWidth;
-        MapPixelHeight = MapHeight * TilePixelHeight;
+        MapPixelWidth = MapWidth * TilePixelWidth; //How Wide the Map is in Pixels
+        MapPixelHeight = MapHeight * TilePixelHeight; // Height in Pixels
 
         System.out.println("Loaded TileMap " + TilemapPath + "\nMapWidth: " + MapWidth + "\nMapHeight: " + MapHeight + "\nTilePixelWidth: " + TilePixelWidth + "\nTilePixelHeight: " + TilePixelHeight + "\nMapPixelWidth: " + MapPixelWidth + "\nMapPixelHeight: " + MapPixelHeight);
 
-        CalculateGraph(gameData);
-        BuildHashmapOfBuildableAreas();
-        return new OrthogonalTiledMapRenderer(tiledMap);
+        CalculateGraph(gameData); //Calculate the graph of the map
+        BuildHashmapOfBuildableAreas(); //Build the hashmap of buildable areas
+        return new OrthogonalTiledMapRenderer(tiledMap); //Return the renderer
     }
 
     private void BuildHashmapOfBuildableAreas() {
-        MapLayer layer = tiledMap.getLayers().get("PlacementLayer");
-        MapObjects mapObjects = layer.getObjects();
+        MapLayer layer = tiledMap.getLayers().get("PlacementLayer"); //Get the layer with the buildable areas
+        MapObjects mapObjects = layer.getObjects(); //Get the objects in the layer
 
-        for (MapObject mapObject : mapObjects) {
-            if (mapObject instanceof RectangleMapObject) {
-                RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject;
+        for (MapObject mapObject : mapObjects) { //For each object in the layer (buildable area)
+            if (mapObject instanceof RectangleMapObject) { //If the object is a rectangle
+                RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject; //Cast the object to a rectangle
                 // now you can get the position of the rectangle like this:
-                Rectangle rectangle = rectangleMapObject.getRectangle();
-                int x = (int) rectangle.getX();
-                int y = (int) rectangle.getY();
+                Rectangle rectangle = rectangleMapObject.getRectangle(); //Get the rectangle
+                int x = (int) rectangle.getX(); //Get the x position of the rectangle
+                int y = (int) rectangle.getY(); //Get the y position of the rectangle
 
-                Tile graphTile = new Tile(x, y, rectangleMapObject.getName());
-                BuildableTiles.put(new Vector2(x, y), graphTile);
+                Tile graphTile = new Tile(x, y, rectangleMapObject.getName()); //Create a new tile with the x and y position and the name of the object
+                BuildableTiles.put(new Vector2(x, y), graphTile); //Add the tile to the hashmap of buildable areas
             }
         }
     }
 
     public void CalculateGraph(GameData gameData) {
-        gameData.GameMapGraph = new GameMapGraph();
-        Tile PreviousTile = null;
-        MapLayer layer = tiledMap.getLayers().get("PathLayer");
-        MapObjects mapObjects = layer.getObjects();
+        gameData.GameMapGraph = new GameMapGraph(); //Create a new graph
+        Tile PreviousTile = null; //Create a new previous tile
+        MapLayer layer = tiledMap.getLayers().get("PathLayer"); //Get the layer with the path
+        MapObjects mapObjects = layer.getObjects(); //Get the objects in the layer
 
-        for (MapObject mapObject : mapObjects) {
-            if (mapObject instanceof RectangleMapObject) {
-                RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject;
+        for (MapObject mapObject : mapObjects) { //For each object in the layer (path)
+            if (mapObject instanceof RectangleMapObject) { //If the object is a rectangle
+                RectangleMapObject rectangleMapObject = (RectangleMapObject) mapObject; //Cast the object to a rectangle
                 // now you can get the position of the rectangle like this:
-                Rectangle rectangle = rectangleMapObject.getRectangle();
-                int x = (int) rectangle.getX();
-                int y = (int) rectangle.getY();
+                Rectangle rectangle = rectangleMapObject.getRectangle(); //Get the rectangle
+                int x = (int) rectangle.getX(); //Get the x position of the rectangle
+                int y = (int) rectangle.getY(); //Get the y position of the rectangle
 
                 // create a graph tile for each object.
-                Tile graphTile = new Tile(x, y, rectangleMapObject.getName());
-                gameData.GameMapGraph.AddTile(graphTile);
+                Tile graphTile = new Tile(x, y, rectangleMapObject.getName()); //Create a new tile with the x and y position and the name of the object
+                gameData.GameMapGraph.AddTile(graphTile); //Add the tile to the graph
 
-                EnemyPath.put(new Vector2(x, y), graphTile);
+                EnemyPath.put(new Vector2(x, y), graphTile); //Add the tile to the hashmap of enemies path
 
-                if (Start == null) {
-                    Start = graphTile;
+                if (Start == null) { //If the start tile is null
+                    Start = graphTile; //Set the start tile to the current tile
                 }
 
-                if (PreviousTile != null) {
-                    gameData.GameMapGraph.ConnectTiles(PreviousTile, graphTile);
+                if (PreviousTile != null) { //If the previous tile is not null
+                    gameData.GameMapGraph.ConnectTiles(PreviousTile, graphTile); //Connect the previous tile to the current tile
                 }
-                PreviousTile = graphTile;
+                PreviousTile = graphTile; //Set the previous tile to the current tile
 
             }
         }
@@ -109,34 +109,13 @@ public class TileMapHelper {
         EnemyPath.put(new Vector2(End.x, End.y), End);
         gameData.GameMapPath = gameData.GameMapGraph.FindPath(Start, End);
     }
-
-
-    public Tile GetEnemyPathTileAt(Vector2 vector) {
-        return EnemyPath.get(vector);
-    }
+    
 
     public boolean IsTileAtPositionAValidBuildableTile(Vector2 vector) {
         return BuildableTiles.containsKey(vector);
     }
 
-    public Tile GetEnemyPathTileAt(float x, float y) {
-        return EnemyPath.get(new Vector2(x, y));
-    }
-
-    // Gets the next tile we should be heading to from the position of the current tile. e.g. input is Tile xy of A => output is Tile B;
-    public Tile GetNextEnemyPathTileFromCurrentTileAt(GameData gameData, float x, float y) {
-        Tile currentTile = EnemyPath.get(new Vector2(x, y));
-        if (currentTile == null) {
-            currentTile = Start;
-        }
-
-        if (gameData.GameMapPath.getCount() - 1 >= currentTile.index + 1) {
-            Tile nextTile = gameData.GameMapPath.get(currentTile.index + 1);
-            return nextTile;
-        } else {
-            return null;
-        }
-    }
+    // Gets the next tile we should be heading to from the position of the current tile. e.g. input is Tile xy of A => output i
 
 
 }
